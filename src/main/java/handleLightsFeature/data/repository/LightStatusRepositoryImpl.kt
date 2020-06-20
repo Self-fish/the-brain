d978a137c75c35114389eb2bf32cdc9d88847de7
pablo <pablo@pablos-MacBook-Pro.local>
2020-06-20 21:53:39 +0200

    Making the logs of Light Feature more useful

--- src/main/java/handleLightsFeature/data/repository/LightStatusRepositoryImpl.kt
+++ src/main/java/handleLightsFeature/data/repository/LightStatusRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package handleLightsFeature.data.repository
 
+import application.logger.LoggerWrapper
 import handleLightsFeature.data.datasource.LightStatusExternalDataSource
 import handleLightsFeature.data.datasource.LightStatusLocalDataSource
 import handleLightsFeature.domain.contracts.repository.LightStatusRepository
@@ -7,13 +8,16 @@ import handleLightsFeature.domain.model.LightStatus
 import org.koin.standalone.KoinComponent
 
 class LightStatusRepositoryImpl(private val localDataSource: LightStatusLocalDataSource,
-                                private val externalDataSource: LightStatusExternalDataSource) :
+                                private val externalDataSource: LightStatusExternalDataSource,
+                                private val logger: LoggerWrapper) :
         KoinComponent, LightStatusRepository {
 
     private val CACHE_TIME_TO_LIVE = 900000 //15 minutes
 
     override fun getLightStatus(): LightStatus {
         if(!isLocalCacheValid()) {
+            logger.fine(this::class.simpleName, "The cache is not valid anymore. Asking for the current " +
+                    "light status")
             localDataSource.updateCache(externalDataSource.getLightStatus())
         }
         return localDataSource.currentLightStatus
